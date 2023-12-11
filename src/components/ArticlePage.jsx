9eb66953b94f7208e28aa24e7867e37b15050d8f
mingfei <mingfei@CPC-mwang-NL26M>
2023-12-11 17:06:51 +0000

    task5 css adjusting

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from "react"
 import { getArticleById } from '../utils/api';
 import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+
 import './ArticlePage.css'
 const ArticlePage = () => {
     const { id } = useParams();
@@ -20,33 +21,26 @@ const ArticlePage = () => {
     }
 
     return (
-        <Container>
-            <Row >
-                <Col>
-                    <h1>{articleData.title}</h1>
-                    <h2>Author: {articleData.author}</h2>
-                </Col>
-            </Row>
-            <Row>
-                <Col/>
-                <Col xs = {8}>
-                    <Image src={articleData.article_img_url}/>
-                     <p>{articleData.body}</p>
-                </Col>
-                <Col/>
-                   
-            </Row>
-            <Row>
-                <Col/>
-                <Col xs={1}>
-                    <Button variant="primary">Votes: {articleData.votes}</Button>
-              </Col>
-              <Col/>
-              <Col xs={1}>
-                    <Button variant="secondary">Comments: {articleData.comment_count}</Button>
-                </Col> <Col/>
-            </Row>
-        </Container>
+        <Container >
+      <Row >
+        <Col xs={12}>
+            <h1 className="article-title">{articleData.title}</h1>
+            <p className="author-text">By {articleData.author}</p>
+        </Col>
+    </Row>
+    <Row>
+    <Col xs={12} md={8} mdOffset={2} style={{ textAlign: 'center', margin:'10px 200px' }}>
+            <img src={articleData.article_img_url} className="article-image"/>
+            <p >{articleData.body}</p>
+        </Col>
+    </Row>
+        <Row className="button-container">  
+            <Button className = 'button' variant="light">Votes: {articleData.votes}</Button>{' '}     
+            <Button className = 'button'variant="light">Comments: {articleData.comment_count}</Button>{' '}
+     
+        </Row>
+    </Container>
+    
     )
 };
 export default ArticlePage
