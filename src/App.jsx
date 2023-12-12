aaa1d692a5ea1c5264a98f96a7982b2dec22cde3
mingfei <mingfei@CPC-mwang-NL26M>
2023-12-12 10:34:55 +0000

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ArticlePage from "./components/ArticlePage";
 import NewsList from "./components/NewsList"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
 
 
 
 function App() {
   return (
       <BrowserRouter>
+       <Navbar/>
         <Routes>
           <Route path='/' element={<NewsList />} />
           <Route path='/articles' element={<NewsList />} />
